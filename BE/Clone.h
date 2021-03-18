1d0bf2c2ad51be3b0f5b5bd4dd9493992aec2fe9
yasminediouri <65313326+yasminediouri@users.noreply.github.com>
2021-03-18 19:36:54 +0100

    Add files via upload

---
+++ BE/Clone.h
@@ -0,0 +1,25 @@
+#idndef _CLONE_H_
+#define _CLONE_H_
+
+#include "Bestiole.h"
+#include "IPrototypeBestiole.h"
+
+class Clone {
+    public : 
+
+    Bestiole* getMultiple() const;
+    Bestiole* getGregaire() const;
+    Bestiole* getPrevoyante() const;
+    Bestiole* getPeureuse() const;
+    Bestiole* getKamikaze() const;
+
+    private : 
+    Bestiole* bestioleMultiple;
+    Bestiole* bestioleGregaire;
+    Bestiole* bestiolePrevoyante;
+    Bestiole* bestiolePeureuse;
+    Bestiole* bestioleKamikaze;
+
+};
+
+#endif
